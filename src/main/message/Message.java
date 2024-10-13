5b5cca7b6e6baed8b472bf2ce001f0a2b38ee93f
CandyXandy <gifmesalad@gmail.com>
2024-10-13 17:40:39 +1030

    change message to a record.

--- src/main/message/Message.java
+++ src/main/message/Message.java
@@ -4,34 +4,8 @@ import member.Members;
 
 /**
  * This class represents a message that is sent between members of the Adelaide Suburbs Council.
- * The message contains a proposal number and a message.
+ * The message contains a proposal number and a message, the sender of the message, and the value
+ * that the sender is proposing.
  */
-public class Message {
-    private final int proposalNum;
-    private final Members sender;
-    private final String message;
-    private final Members value;
-
-        public Message(int proposalNum, Members sender, String message, Members value) {
-            this.sender = sender;
-            this.proposalNum = proposalNum;
-            this.message = message;
-            this.value = value;
-        }
-
-        public int getProposalNum() {
-            return proposalNum;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public Members getSender() {
-            return sender;
-        }
-
-        public Members getValue() {
-            return value;
-        }
+public record Message(int proposalNum, Members sender, String message, Members value) {
 }
